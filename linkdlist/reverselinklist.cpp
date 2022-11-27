7e6fd98075d0574d056ac355e3531a144972e674
Ashis Banerjee <58388611+Ashis101@users.noreply.github.com>
2022-11-27 22:23:53 +0530

    solved to gfg

--- linkdlist/reverselinklist.cpp
+++ linkdlist/reverselinklist.cpp
@@ -1,5 +1,38 @@
 #include<iostream>
 
-int main(){
+class Node{
+    public:
+    int data;
+    Node* next;
     
+    Node(int d){
+        this->data=d;
+        this->next=NULL;
+    };
+};
+
+void reverse(Node* &head){
+    if(head == NULL || head->next == NULL){
+            return head;
+        }
+        
+        Node* prev=NULL;
+        Node* curr=head;
+        
+        while(curr != NULL){
+            Node* forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+            
+        }
+        return prev;
+}
+
+int main(){
+
+    Node* head=new Node(10);
+
+    reverse(head);
+
 }
